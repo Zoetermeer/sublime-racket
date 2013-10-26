972cd4a29fb8726aac4e62b71265b24c3a867bda
James Swaine <james.swaine@gmail.com>
2013-10-25 22:44:34 -0500

    Add support for some LaTeX keybinding macros

--- Main.py
+++ Main.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-import sublime, sublime_plugin
+import sublime, sublime_plugin, re
 
 def matches(c, cs):
 	for ch in cs:
@@ -91,10 +91,68 @@ class MoveSexpLeftCommand(sublime_plugin.TextCommand):
 		extend = args['extend']
 		move_to_open_paren(self.view, extend=extend)
 
+class MacroKB(object):
+	def __init__(self, pat, key):
+		self.pat = pat
+		self.regex = re.compile(pat)
+		self.key = key
+
+class TryExpandMacroKbCommand(sublime_plugin.TextCommand):
+	def __init__(self, view):
+		self.view = view;
+		self.macros = [
+				MacroKB(r"\\Downarrow", u"⇓"), 
+				MacroKB(r"\\nwarrow", u"↖"), 
+				MacroKB(r"\\downarrow", u"↓"), 
+				MacroKB(r"\\Rightarrow", u"⇒"), 
+				MacroKB(r"\\rightarrow", u"→"), 
+				MacroKB(r"\\mapsto", u"↦"), 
+				MacroKB(r"\\aleph", u"א"), 
+				MacroKB(r"\\prime", u"′"), 
+				MacroKB(r"\\emptyset", u"∅"), 
+				MacroKB(r"\\sharp", u"♯"), 
+				MacroKB(r"\\flat", u"♭"), 
+				MacroKB(r"\\neg", u"¬"), 
+				MacroKB(r"\\forall", u"∀"), 
+				MacroKB(r"\\exists", u"∃"), 
+				MacroKB(r"\\infty", u"∞"), 
+				MacroKB(r"\\alpha", u"α"), 
+				MacroKB(r"\\theta", u"θ"), 
+				MacroKB(r"\\tau", u"τ"), 
+				MacroKB(r"\\beta", u"β"), 
+				MacroKB(r"\\vartheta", u"θ"), 
+				MacroKB(r"\\pi", u"π"), 
+				MacroKB(r"\\upsilon", u"υ"), 
+				MacroKB(r"\\gamma", u"γ"), 
+				MacroKB(r"\\lambda", u"λ"), 
+				MacroKB(r"\\smiley", u"☺"), 
+				MacroKB(r"\\frownie", u"☹")
+					  ]
+		self.longest_pat = 0
+		for mkb in self.macros:
+			self.longest_pat = max(len(mkb.pat), self.longest_pat)
+
+	def run(self, edit):
+		for r in self.view.sel():
+			tsz = min(self.longest_pat, r.b)
+			text = self.view.substr(sublime.Region(r.b - tsz, r.b)).encode("utf-8")
+			for mac in self.macros:
+				m = mac.regex.search(text)
+				if m != None:
+					start = r.b - tsz
+					to_replace = sublime.Region(start + m.start(), start + m.end())
+					self.view.replace(edit, to_replace, mac.key)
+					break
+
 class InsertLambdaCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		for r in self.view.sel():
 			self.view.insert(edit, r.b, u"λ")
+
+class InsertFrownieCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		for r in self.view.sel():
+			self.view.insert(edit, r.b, u"☹")
 
 class InsertParenCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
@@ -107,7 +165,6 @@ class InsertParenCommand(sublime_plugin.TextCommand):
 				lambda p: p - 1, 
 				terminate_at_level = 0, 
 				level = 1) - 1
-			print "Character at " + str(matching_brace_pos) + ":" + self.view.substr(matching_brace_pos)
 			if self.view.substr(matching_brace_pos) == "[":
 				self.view.insert(edit, r.b, "]")
 			else:
@@ -256,7 +313,7 @@ class HighlightSExp(sublime_plugin.EventListener):
 					add_reg(sublime.Region(peek(node.children).end, node.end), node.level)
 
 			subdivide(root)
-			view.add_regions("ParenHighlight1", ones, "sexp.one")
+			view.add_regions("ParenHighlight1", ones, "sexp.one", "dot")
 			view.add_regions("ParenHighlight2", twos, "sexp.two")
 			view.add_regions("ParenHighlight3", threes, "sexp.three")
 			view.add_regions("ParenHighlight4", fours, "sexp.four")
